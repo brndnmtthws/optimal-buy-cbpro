2c0de0bf1f6738eb5020a0244736f2f8ca879d5a
Brenden Matthews <brenden@diddyinc.com>
2018-11-26 10:57:53 -0500

    Packaging fixes.

---
+++ optimal_buy_cbpro/__init__.py
@@ -0,0 +1,1 @@
+name = "optimal-buy-cbpro"
--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ with open('README.md') as f:
 
 setup(
     name='optimal_buy_cbpro',
-    version='1.1.5',
+    version='1.1.6',
     description='Buy the coins, optimally!',
     long_description=readme,
     long_description_content_type="text/markdown",
     author='Brenden Matthews',
     author_email='brenden@diddyinc.com',
     url='https://github.com/brndnmtthws/optimal-buy-cbpro',
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(),
     entry_points={
         'console_scripts':
             ['optimal-buy-cbpro=optimal_buy_cbpro.optimal_buy_cbpro:main'],
     },
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Public Domain",
         "Operating System :: OS Independent",
-    ),
+    ],
 )
