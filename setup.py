9c61ec8e94c8b5e566989bb9e925d64ba35d9a1d
Brenden Matthews <brenden@diddyinc.com>
2018-11-26 13:35:18 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requires = [
 
 setup(
     name='optimal_buy_cbpro',
-    version='1.1.12',
+    version='1.1.13',
     description='Buy the coins, optimally!',
     long_description=readme,
     long_description_content_type="text/markdown",
