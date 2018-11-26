a11f4ab1e06c91a568df36ab37b90de38a21aa68
Brenden Matthews <brenden@diddyinc.com>
2018-11-26 08:22:27 -0500

    Fix pypi package settings.

--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='optimal_buy_gdax',
-    version='1.1.3',
+    version='1.1.4',
     description='Buy the coins, optimally!',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='Brenden Matthews',
     author_email='brenden@diddyinc.com',
     url='https://github.com/brndnmtthws/optimal-buy-gdax',
-    license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
         'console_scripts':
             ['optimal-buy-gdax=optimal_buy_gdax.optimal_buy_gdax:main'],
     },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ),
 )
